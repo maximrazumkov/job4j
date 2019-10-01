7a5ec16f7d5ceb84ea21fd2002f07b07ada5738d
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-10-01 13:33:02 +0300

    JavaDoc. Code convention[#181393]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,27 +1,56 @@
 package ru.job4j.calculator;
 
+/**
+ * Calculator
+ * This calculator implements addition, multiplication, subtraction, division
+ * @author m.razumkov
+ */
 public class Calculator {
 
+    /**
+     * Addition and print to console
+     * @param first term
+     * @param second term
+     */
     public static void add(double first, double second) {
         double result = first + second;
         System.out.println(first + "+" + second + " = " + result);
     }
 
+    /**
+     * Division and print to console
+     * @param first dividend
+     * @param second divider
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + "/" + second + " = " + result);
     }
 
+    /**
+     * Multiplication and print to console
+     * @param first factor
+     * @param second factor
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + "*" + second + " = " + result);
     }
 
+    /**
+     * Subtraction and print to console
+     * @param first minuend
+     * @param second subtrahend
+     */
     public static void subtract(double first, double second) {
         double result = first - second;
         System.out.println(first + "-" + second + " = " + result);
     }
 
+    /**
+     * Test calculator's methods
+     * @param args command line options
+     */
     public static void main(String[] args) {
         add(1, 1);
         multiply(4, 4);
---
+++ chapter_001/src/main/java/ru/job4j/calculator/package-info.java
@@ -0,0 +1,9 @@
+package ru.job4j.calculator;
+
+/**
+ * Package for calculate task.
+ *
+ * @author m.razumkov (maxim.razumkov@gmail.com)
+ * @version 1.0
+ * @since 01.10.2019
+ */
