c9c85ab0d7b17f05d5b9a6cfbd183dd4d186a9f1
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-10-02 12:21:15 +0300

    Крест в псевдографике. [#181399]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,27 @@
+package ru.job4j.loop;
+
+public class Slash {
+
+    public static void draw(int size) {
+        for (int i = 0; i < size; ++i) {
+            for (int j = 0; j < size; ++j) {
+                boolean left = i == j ? true : false;
+                boolean right = (i + j + 1) == size;
+                if (left) {
+                    System.out.print("0");
+                } else if (right) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        draw(3);
+        draw(4);
+        draw(5);
+    }
+}
