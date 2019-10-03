b6d7390e9cba2a05f3584799a6d5db3d41adb8ee
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-10-03 10:57:33 +0300

    Поиск индекса в диапазоне.[#181413]

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,8 +1,14 @@
 package ru.job4j.array;
 
 public class FindLoop {
+
     public int indexOf(int[] data, int el) {
-        for (int i = 0; i < data.length; ++i) {
+        return indexOf(data, el, 0, data.length - 1);
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int endIndex = data.length - 1 < finish ? data.length - 1 : finish;
+        for (int i = start; i <= endIndex; ++i) {
             if (data[i] == el) {
                 return i;
             }
--- chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/FindLoopTest.java
@@ -7,6 +7,51 @@ import static org.junit.Assert.assertThat;
 
 public class FindLoopTest {
     @Test
+    public void whenNotFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 5;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFind3() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFinishMoreInputLength() {
+        int[] input = new int[] {5, 2, 10, 2, 4};
+        int value = 2;
+        int start = 2;
+        int finish = 10;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = 3;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenArrayInEmpty() {
+        FindLoop find = new FindLoop();
+        int[] input = new int[] {};
+        int value = 2;
+        int start = 2;
+        int finish = 10;
+        int result = FindLoop.indexOf(input, value, start, finish);
+        int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
         int[] input = new int[] {5, 10, 3};
