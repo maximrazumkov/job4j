da73dae47105db49e990aaa1178eb418509b4ad3
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-10-06 15:41:13 +0300

    1.5. Поля объекта[#181528]

--- chapter_001/src/main/java/ru/job4j/oop/Cat.java
+++ chapter_001/src/main/java/ru/job4j/oop/Cat.java
@@ -2,6 +2,22 @@ package ru.job4j.oop;
 
 public class Cat {
 
+    private String food;
+
+    private String name;
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println(name + " ate " + food);
+    }
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
     public String sound() {
         String voice = "may-may";
         return voice;
@@ -11,5 +27,16 @@ public class Cat {
         Cat peppy = new Cat();
         Cat sparky = new Cat();
         String say = peppy.sound();
+
+        System.out.println("There are gav's food.");
+        Cat gav = new Cat();
+        gav.giveNick("Gav");
+        gav.eat("kotleta");
+        gav.show();
+        System.out.println("There are black's food.");
+        Cat black = new Cat();
+        black.giveNick("Black Cat");
+        black.eat("fish");
+        black.show();
     }
 }
