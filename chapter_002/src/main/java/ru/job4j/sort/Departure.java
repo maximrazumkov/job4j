809f94280015d7291c87016d5468eaaab06fdc70
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-11-10 18:50:29 +0300

    Контрольные вопросы.[#181471] Отсортировать департаменты [#34352]

---
+++ chapter_002/src/main/java/ru/job4j/sort/Departure.java
@@ -0,0 +1,69 @@
+package ru.job4j.sort;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Objects;
+
+public class Departure {
+    private List<String> orgs;
+
+    public Departure() {
+    }
+
+    public Departure(List<String> orgs) {
+        this.orgs = orgs;
+    }
+
+    public List<String> getOrgs() {
+        return orgs;
+    }
+
+    public void setOrgs(List<String> orgs) {
+        this.orgs = orgs;
+    }
+
+    public void abs() {
+        Collections.sort(this.orgs, (str1, str2) -> str1.compareTo(str2));
+    }
+
+    public void desc() {
+        Collections.sort(this.orgs, (str1, str2) -> {
+            int result = 0;
+            int size = Math.min(str1.length(), str2.length());
+            for (int i = 0; (i < size) && (result == 0); ++i) {
+                char leftSimbol = str1.charAt(i);
+                char rightSimbol = str2.charAt(i);
+                result = Character.compare(rightSimbol, leftSimbol);
+            }
+            if (result == 0) {
+                result = str1.compareTo(str2);
+            }
+            return result;
+        });
+    }
+
+    public List<String> fillGaps(List<String> orgs) {
+        this.orgs.addAll(0, orgs);
+        return this.orgs;
+    }
+
+    @Override
+    public String toString() {
+        return "Departure{" +
+                "orgs=" + orgs +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Departure departure = (Departure) o;
+        return Objects.equals(orgs, departure.orgs);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(orgs);
+    }
+}
---
+++ chapter_002/src/test/java/ru/job4j/sort/DepartureTest.java
@@ -0,0 +1,55 @@
+package ru.job4j.sort;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class DepartureTest {
+
+    private Departure departure;
+
+    @Before
+    public void Departure() {
+        this.departure = new Departure();
+        List<String> departures = new ArrayList<String>(Arrays.asList(
+                "K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2", "K2\\SK1", "K1"
+        ));
+        this.departure.setOrgs(departures);
+    }
+
+    @Test
+    public void whenAbsSort() {
+        List<String> expect = Arrays.asList(
+                "K1", "K1\\SK1", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K1\\SK2", "K2", "K2\\SK1", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2"
+        );
+        this.departure.abs();
+        List<String> result = departure.getOrgs();
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenDescSort() {
+        List<String> expect = Arrays.asList(
+                "K2", "K2\\SK1", "K2\\SK1\\SSK2", "K2\\SK1\\SSK1", "K1", "K1\\SK2", "K1\\SK1", "K1\\SK1\\SSK2", "K1\\SK1\\SSK1"
+        );
+        this.departure.desc();
+        List<String> result = departure.getOrgs();
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenFillGaps() {
+        List<String> expect = Arrays.asList(
+                "K1", "K1\\SK1", "K1\\SK2", "K1\\SK1\\SSK1", "K1\\SK1\\SSK2", "K2", "K2\\SK1\\SSK1", "K2\\SK1\\SSK2", "K2\\SK1", "K1"
+        );
+        this.departure.fillGaps(Arrays.asList("K1"));
+        List<String> result = departure.getOrgs();
+        assertThat(result, is(expect));
+    }
+}
