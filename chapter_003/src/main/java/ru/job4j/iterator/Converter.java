e5ea1380eb54ad646551d2a9824867dfd557fb17
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-11-12 13:59:34 +0300

    5.1.4. Создать convert(Iterator<Iterator>)[#195996]

--- chapter_003/src/main/java/ru/job4j/iterator/Converter.java
+++ chapter_003/src/main/java/ru/job4j/iterator/Converter.java
@@ -5,42 +5,31 @@ import java.util.NoSuchElementException;
 
 public class Converter {
     Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
-        return new IteratorOfIterator(it);
-    }
+        return new Iterator<Integer>() {
+            private Iterator<Integer> subIt;
 
-    private class IteratorOfIterator implements Iterator<Integer> {
-        private final Iterator<Iterator<Integer>> it;
-        private Iterator<Integer> subIt;
-
-        public IteratorOfIterator(Iterator<Iterator<Integer>> it) {
-            this.it = it;
-            if (it.hasNext()) {
-                subIt = it.next();
-            }
-        }
-
-        @Override
-        public boolean hasNext() {
-            boolean result = subIt != null;
-            while (result) {
-                result = subIt.hasNext();
-                if (subIt.hasNext()) {
-                    break;
+            @Override
+            public boolean hasNext() {
+                boolean result = true;
+                while (result) {
+                    if (subIt != null && subIt.hasNext()) {
+                        break;
+                    }
+                    result = it.hasNext();
+                    if (result) {
+                        subIt = it.next();
+                    }
                 }
-                result = it.hasNext();
-                if (result) {
-                    subIt = it.next();
-                }
+                return result;
             }
-            return result;
-        }
 
-        @Override
-        public Integer next() {
-            if (!hasNext()) {
-                throw new NoSuchElementException();
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                return subIt.next();
             }
-            return subIt.next();
-        }
+        };
     }
 }
