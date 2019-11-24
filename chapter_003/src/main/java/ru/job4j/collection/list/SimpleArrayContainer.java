4e11fbcf53e62c78820e83c9a0176fb809d2f7e4
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-11-24 12:56:37 +0300

    5.3.1. Создать динамический список на базе массива.[#195978]

---
+++ chapter_003/src/main/java/ru/job4j/collection/list/SimpleArrayContainer.java
@@ -0,0 +1,70 @@
+package ru.job4j.collection.list;
+
+import java.util.*;
+
+public class SimpleArrayContainer<T> implements Iterable<T> {
+
+    private int size = 5;
+
+    private int modCount = 0;
+
+    private int index = 0;
+
+    private Object[] elements;
+
+    public SimpleArrayContainer() {
+        elements = new Object[size];
+    }
+
+    public SimpleArrayContainer(int qtyElements) {
+        elements = qtyElements > 0 ? new Object[qtyElements] : new Object[size];
+    }
+
+    public void add(T element) {
+        if (index == size) {
+            size *= 2;
+            elements = Arrays.copyOf(elements, size);
+        }
+        elements[index++] = element;
+        ++modCount;
+    }
+
+    public T get(int index) {
+        return (T) elements[index];
+    }
+
+    public int size() {
+        return index;
+    }
+
+    @Override
+    public Iterator<T> iterator() {
+        return new Itr();
+    }
+
+    private class Itr implements Iterator<T> {
+
+        private int expectedModCount = modCount;
+        private int idx = 0;
+
+        @Override
+        public boolean hasNext() {
+            boolean result = true;
+            if (expectedModCount != modCount) {
+                throw new ConcurrentModificationException();
+            }
+            if (idx == index) {
+                result = false;
+            }
+            return result;
+        }
+
+        @Override
+        public T next() {
+            if (!hasNext()) {
+                throw new NoSuchElementException();
+            }
+            return (T) elements[idx++];
+        }
+    }
+}
