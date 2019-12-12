3a25f7eac79e2e5c10451813f971bc3855f880bb
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-12-12 14:54:15 +0300

    Сравнения объектов

---
+++ chapter_003/src/main/java/ru/job4j/collection/map/UserUtil.java
@@ -0,0 +1,18 @@
+package ru.job4j.collection.map;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserUtil {
+
+    public static void main(String[] args) {
+        Map<Usr, Object> users = new HashMap<>();
+        Calendar cal = Calendar.getInstance();
+        Usr user1 = new Usr("Jon", 18, cal);
+        Usr user2 = new Usr("Jon", 18, cal);
+        users.put(user1, new Object());
+        users.put(user2, new Object());
+        System.out.println(users);
+    }
+}
