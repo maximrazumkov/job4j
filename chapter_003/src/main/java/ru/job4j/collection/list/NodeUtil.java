3663654ff693223265f12090b4f90f0da76b546f
Maxim Razumkov <maxim.razumkov@gmail.com>
2019-12-12 14:02:47 +0300

    5.3.4. Задан связанный список. Определить цикличность.[#195981]. Поправил замечания. Реализован алгортим флойда

--- chapter_003/src/main/java/ru/job4j/collection/list/NodeUtil.java
+++ chapter_003/src/main/java/ru/job4j/collection/list/NodeUtil.java
@@ -3,22 +3,28 @@ package ru.job4j.collection.list;
 public class NodeUtil {
 
     public static boolean hasCycle(Node first) {
-        boolean isCycle = false;
-        Node current = first;
-        SimpleQueueFIFO<Node> queue = new SimpleQueueFIFO<>();
-        while ((!isCycle) && (current != null)) {
-            queue.push(current);
-            current = current.next;
-            int size = queue.size();
-            for (int i = 0; i < size; ++i) {
-                Node node = queue.poll();
-                if (node == current) {
-                    isCycle = true;
-                    break;
-                }
-                queue.push(node);
+        boolean result = true;
+        if (first == null)
+            result = false;
+        Node tortoise = first;
+        Node hare = first;
+        while (result) {
+            tortoise = tortoise.next;
+            if (hare.next != null) {
+                hare = hare.next.next;
+            } else {
+                result = false;
+                break;
+            }
+            if ((tortoise == null) || (hare == null)) {
+                result = false;
+                break;
+            }
+            if (tortoise == hare) {
+                result = true;
+                break;
             }
         }
-        return isCycle;
+        return result;
     }
 }
